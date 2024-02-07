9dc771ecc0cbe6418918f6bc71911f43c5612c13
Ryan Shaw-Harrison <srazash@protonmail.com>
2024-02-07 20:35:34 +0000

    Containers 2 - lists

--- containers2.cpp
+++ containers2.cpp
@@ -1,13 +1,4 @@
-#include <cstdlib>
 #include <iostream>
-#include <string>
-#include <limits>
-#include <vector>
-#include <sstream>
-#include <numeric>
-#include <ctime>
-#include <cmath>
-#include <fstream>
 #include <iterator>
 #include <deque>
 #include <list>
@@ -65,7 +56,7 @@ int main(int argc, char** argv) {
       cout << i << endl;
     }
 
-    // when create a new deque we can specify what value to insert and how many times
+    // when creating a new deque we can specify what value to insert and how many
     deque<int> mynewdeque(3, 23); // three elements, all of which are 23
 
     // we can swap the elements between dequeues
@@ -78,6 +69,108 @@ int main(int argc, char** argv) {
 
     // we can also delete all the values in the deque
     mynewdeque.clear();
+
+    // LISTS
+    // lists are very efficient at inserting, moving and extracting elements
+    // but lack direct access to the elements that arrays and vectors offer
+
+    int numbers[5] = {1, 2, 3, 4, 5};
+    list<int> mylist; // create a list
+    mylist.insert(mylist.begin(), numbers, numbers + 5); // we can insert values from an array
+
+    cout << "Size of mylist: " << mylist.size() << endl;
+    for (auto i: mylist) {
+      cout << i << endl;
+    }
+
+    mylist.assign({10, 11, 12}); // we can assign which will replace all existing values
+
+    cout << "Size of mylist: " << mylist.size() << endl;
+    for (auto i: mylist) {
+      cout << i << endl;
+    }
+
+    // we can push values to the front or end of the list
+    mylist.push_back(13);
+    mylist.push_front(9);
+
+    cout << "Size of mylist: " << mylist.size() << endl;
+    for (auto i: mylist) {
+      cout << i << endl;
+    }
+
+    // it's not possible to directly access the elements of a list like we can with an
+    // array or vector BUT we can use an interator to access the elements
+    list<int>::iterator listitr = mylist.begin(); // iterator points to the 1st element
+    advance(listitr, 1); // advance by 1 element
+
+    cout << "The 2nd item in mylist is: " << *listitr << endl; // dereference, as an inerator is a pointer
+
+    // set the iterator back to the first element and replace it using insert()
+    listitr = mylist.begin();
+    mylist.insert(listitr, 1);
+
+    cout << "Size of mylist: " << mylist.size() << endl;
+    for (auto i: mylist) {
+      cout << i << endl;
+    }
+
+    // we can erase a value
+    mylist.erase(listitr);
+
+    cout << "Size of mylist: " << mylist.size() << endl;
+    for (auto i: mylist) {
+      cout << i << endl;
+    }
+
+    // we can also erase a range of values
+    listitr = mylist.begin();
+    list<int>::iterator listitr2 = mylist.begin();
+    advance(listitr2, 2);
+    mylist.erase(listitr, listitr2);
+
+    cout << "Size of mylist: " << mylist.size() << endl;
+    for (auto i: mylist) {
+      cout << i << endl;
+    }
+
+    // and we can pop the first and last values off the list
+    mylist.pop_front();
+    mylist.pop_back();
+
+    cout << "Size of mylist: " << mylist.size() << endl;
+    for (auto i: mylist) {
+      cout << i << endl;
+    }
+
+    int numbers2[6] = {99, 98, 97, 96, 95, 95}; // duplicate value is intentional!
+    list<int> mylist2;
+    mylist2.insert(mylist2.begin(), numbers2, numbers2 + 6);
+
+    // we can sort a list, or reverse a list
+    mylist2.sort();
+    mylist2.reverse();
+
+    // we can remove consecutive duplicate values
+    mylist2.unique();
+
+    // we can remove a specific value
+    mylist2.remove(95);
+    mylist2.remove_if( // we can use remove_if() to conditionally remove values
+        [](int x){ return (x % 2) != 0; }); // remove odd values
+
+    cout << "Size of mylist2: " << mylist2.size() << endl;
+    for (auto i: mylist2) {
+      cout << i << endl;
+    }
+    
+    // we can merge lists
+    mylist.merge(mylist2);
+
+    cout << "Size of mylist: " << mylist.size() << endl;
+    for (auto i: mylist) {
+      cout << i << endl;
+    }
 
     return 0;
 }
